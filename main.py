6a2dd24c728338bdab1a42d76b0d16d386427765
imSarahOwl <cosmicusesmail@proton.me>
2025-03-30 22:39:55 -0300

    added some styling(we are back babyyy)

--- main.py
+++ main.py
@@ -6,26 +6,24 @@ import utils.get_apps
 class MainWindow(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        
+        # initial window stuff
         self.layout = QtWidgets.QVBoxLayout(self)
         self.entry = QtWidgets.QLineEdit()
         self.entry.setPlaceholderText("")
         self.entry.setFixedSize(700, 100)      
         self.entry.textChanged.connect(self.finder)
 
+        # define theme
+        with open('./style/style.qss', "r") as f:
+            self.setStyleSheet(f.read())
 
         self.layout.setContentsMargins(0, 0, 0, 0)  
         self.layout.addWidget(self.entry)
 
-
-        
-    
-
     # equivalent to escclose()
     def keyPressEvent(self, event: QtGui.QKeyEvent):
         if event.key() == Qt.Key_Escape:
-            sys.exit(app.exec()
-)
+            sys.exit(app.exec())
     def finder(self):
         print(self.entry.text())
         filtered_apps = [app.getName() for app in utils.get_apps.get_apps() if app.getName().lower().startswith(self.entry.text().lower())]
