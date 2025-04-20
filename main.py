bb00f97ee869cbac1d58ab37e949692a88c432a4
imSarahOwl <cosmicusesmail@proton.me>
2025-04-20 19:08:21 -0300

    added a tray and a really pretty icon
    at least the icon's not ai :3

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
 import sys
 from PySide6 import QtWidgets
+from PySide6.QtGui import QIcon, QAction
+
 from ui.launcher import MainWindow
 
 
+def new_window():
+    w = MainWindow()
+    w.show()
+    open_windows.append(w)
+
+
+# biggest gambiarra of my life (sim sou br :p)
+
+open_windows = []
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
-    widget = MainWindow()
-    widget.show()
+    app.setQuitOnLastWindowClosed(False)
+
+    # start to define the tray
+    icon = QIcon("icon.png")
+    tray = QtWidgets.QSystemTrayIcon()
+    tray.setIcon(icon)
+    tray.setToolTip("starlit!")
+
+    # ok now we have a menu :p
+    menu = QtWidgets.QMenu()
+    open = QAction("open")
+    open.triggered.connect(new_window)
+    menu.addAction(open)
+
+    quit = QAction("quit")
+    quit.triggered.connect(app.quit)
+    menu.addAction(quit)
+
+    tray.setContextMenu(menu)
+    tray.show()
     sys.exit(app.exec())
