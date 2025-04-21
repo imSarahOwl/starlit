a3a745cb862576f40eb34d357abb2374349751d2
imSarahOwl <cosmicusesmail@proton.me>
2025-04-20 22:09:58 -0300

    made it only open 1 instance
    this is using qlocalserver and qlocalsocket, love it :3

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import sys
-from PySide6 import QtWidgets
+from PySide6 import QtCore, QtWidgets
+from PySide6.QtNetwork import QLocalServer, QLocalSocket
 from PySide6.QtGui import QIcon, QAction
 
 from ui.launcher import MainWindow
+
+APP_ID = "xyz.missowl.Starlit"
+
+
+def another_running_instance():
+    socket = QLocalSocket()
+    socket.connectToServer(APP_ID)
+    is_running = socket.waitForConnected(100)
+    socket.abort()
+    return is_running
+
+
+def create_server():
+    server = QLocalServer()
+    server.removeServer(APP_ID)
+    server.listen(APP_ID)
+    return server
 
 
 def new_window():
@@ -16,7 +34,12 @@ def new_window():
 open_windows = []
 
 if __name__ == "__main__":
+    if another_running_instance():
+        print("it's already running >:p")
+        sys.exit(0)
+
     app = QtWidgets.QApplication([])
+    server = create_server()
     app.setQuitOnLastWindowClosed(False)
 
     # start to define the tray
