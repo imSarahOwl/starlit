54fd77e63d258b9efee32b5bee7c11aa4f24ddda
imSarahOwl <cosmicusesmail@proton.me>
2025-04-21 00:24:30 -0300

    moved server logic to another file, added help and toggle with argparse

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
-import sys
+import sys, argparse
 from PySide6 import QtCore, QtWidgets
-from PySide6.QtNetwork import QLocalServer, QLocalSocket
 from PySide6.QtGui import QIcon, QAction
-
+from utils.server import another_running_instance, create_server, send_message
 from ui.launcher import MainWindow
 
 APP_ID = "xyz.missowl.Starlit"
 
+parser = argparse.ArgumentParser(
+    description="A fast and extendable application launcher with plugin support"
+)
+parser.add_argument("--toggle", action="store_true", help="launch new starlit window")
 
-def another_running_instance():
-    socket = QLocalSocket()
-    socket.connectToServer(APP_ID)
-    is_running = socket.waitForConnected(100)
-    socket.abort()
-    return is_running
+args = parser.parse_args()
 
 
-def create_server():
-    server = QLocalServer()
-    server.removeServer(APP_ID)
-    server.listen(APP_ID)
-    return server
+def on_new_connection():
+    socket = server.nextPendingConnection()
+    if socket and socket.waitForReadyRead(1000):
+        message = socket.readAll().data().decode()
+        match message:
+            case "toggle":
+                new_window()
+            case _:
+                print(message)
+        socket.close()
 
 
 def new_window():
@@ -29,17 +32,20 @@ def new_window():
     open_windows.append(w)
 
 
-# biggest gambiarra of my life (sim sou br :p)
-
 open_windows = []
 
 if __name__ == "__main__":
+    if args.toggle and another_running_instance():
+        send_message("toggle")
+        sys.exit(0)
+
     if another_running_instance():
         print("it's already running >:p")
         sys.exit(0)
 
     app = QtWidgets.QApplication([])
     server = create_server()
+    server.newConnection.connect(on_new_connection)
     app.setQuitOnLastWindowClosed(False)
 
     # start to define the tray
