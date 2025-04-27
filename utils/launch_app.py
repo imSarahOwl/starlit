f24255c771b4ab838f8bc995bfa3da7a9563a87b
imSarahOwl <sarah@missowl.xyz>
2025-04-27 20:16:09 -0300

    split the exec to fix flatpak apps

--- utils/launch_app.py
+++ utils/launch_app.py
@@ -2,7 +2,12 @@ import os, platform, subprocess
 
 def launch_app(exec):
     if platform.system() == "Linux":
-        subprocess.Popen(exec.replace("%F", "").replace("%f", "").replace("%U", "").replace("%u", "").strip())
+        to_remove = ["%F", "%f", "%U", "%u", "@@u", "@@"]
+        for word in to_remove:
+            exec = exec.replace(word, "")
+
+        execie = exec.strip().split()
+        subprocess.Popen(execie)
     
     if platform.system() == "Windows":
         subprocess.Popen(rf"explorer shell:appsFolder\{exec}")
