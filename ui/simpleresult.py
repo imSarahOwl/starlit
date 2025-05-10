693bc93043c96835158189a5ead1dda129e63a0c
imSarahOwl <sarah@missowl.xyz>
2025-05-10 20:23:45 -0300

    fixed the result widget up

--- ui/simpleresult.py
+++ ui/simpleresult.py
@@ -1,24 +1,23 @@
 from PySide6 import QtWidgets, QtCore, QtGui
+from xdg.IconTheme import getIconPath
 
 
 class SimpleResult(QtWidgets.QWidget):
     def __init__(self, name, icon):
         super().__init__()
-        self.setFixedSize(700, 100)
-        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
-        self.setWindowFlag(QtCore.Qt.WindowStaysOnTopHint)
-
         self.layout = QtWidgets.QHBoxLayout(self)
-        self.svgicon = QtGui.QIcon(icon)
-        self.svgicon.setFixedSize(48, 48)
+        print(icon)
+        self.icon = QtGui.QIcon(getIconPath(icon, size=48))
+        self.iconlabel = QtWidgets.QLabel()
+        self.iconlabel.setPixmap(self.icon.pixmap(48, 48))
 
         self.label = QtWidgets.QLabel(name)
 
-        self.layout.addWidget(self.svgicon)
+        self.layout.addWidget(self.iconlabel)
         self.layout.addWidget(self.label)
         self.layout.addStretch()
 
         self.setStyleSheet("""
-            padding-left: 25px;
-            font-size: 24px
+            font-size: 24px;
+            background-color: rgba(254, 254, 254, 204)
         """)
