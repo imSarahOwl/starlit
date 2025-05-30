c75fb3283f696bd1fa0a75a854c60a536ec4e2d6
imSarahOwl <sarah@missowl.xyz>
2025-05-30 18:43:03 -0300

    starting gtk!!!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use gtk::{Application, glib};
+use gtk::{ApplicationWindow, prelude::*};
+
+const APP_ID: &str = "xyz.missowl.Starlit";
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder().application_id(APP_ID).build();
+
+    app.connect_activate(build_ui);
+    app.run()
+}
+
+fn build_ui(app: &Application) {
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Starlit")
+        .default_height(100)
+        .default_width(700)
+        .resizable(false)
+        .build();
+
+    window.set_size_request(700, 100);
+    let entry = gtk::Entry::new();
+    window.add(&entry);
+    window.show_all();
 }
